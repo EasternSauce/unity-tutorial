756193aaea5f64c520c24c035ef3d71a7a117884
EasternSauce <frequentmerger@gmail.com>
2025-05-11 00:03:23 +0200

    tutorial part 13 complete

---
+++ Assets/InventoryController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [HideInInspector] public ItemGrid selectedItemGrid;
+
+    private void Update()
+    {
+        if(selectedItemGrid == null) { return; }
+
+        if(Input.GetMouseButtonDown(0)) {
+            Debug.Log(selectedItemGrid.GetTileGridPosition(Input.mousePosition));
+        }
+    }
+}
