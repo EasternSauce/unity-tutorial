9a4303ded132b00d0b02763971d36fe408cae574
EasternSauce <frequentmerger@gmail.com>
2025-05-14 19:43:29 +0200

    tutorial part 19 complete

--- Assets/ItemChestInteractableObject.cs
+++ Assets/ItemChestInteractableObject.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 
 public class ItemChestInteractableObject : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] ItemData dropItem;
+
+    [SerializeField] float itemDropRange = 2f;
+
+    bool isOpened = false;
 
     private void Start()
     {
@@ -12,6 +18,19 @@ public class ItemChestInteractableObject : MonoBehaviour
 
     public void OpenChest(Inventory inventory)
     {
+        if (isOpened == true) { return; }
         animator.SetBool("Open", true);
+        ItemSpawnManager.instance.SpawnItem(SelectRandomPosition(), dropItem);
+        isOpened = true;
+    }
+
+    private Vector3 SelectRandomPosition()
+    {
+        Vector3 pos = transform.position;
+
+        pos += Vector3.right * UnityEngine.Random.Range(-itemDropRange, itemDropRange);
+        pos += Vector3.forward * UnityEngine.Random.Range(-itemDropRange, itemDropRange);
+
+        return pos;
     }
 }
---
+++ Assets/ItemSpawnManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ItemSpawnManager : MonoBehaviour
+{
+    public static ItemSpawnManager instance;
+
+    [SerializeField] LayerMask terrainLayerMask;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] GameObject itemPrefab;
+
+    public void SpawnItem(Vector3 position, ItemData itemToSpawn)
+    {
+        position += Vector3.up * 20;
+
+        Ray findSurfaceRay = new Ray(position, Vector3.down);
+        RaycastHit hit;
+
+        if (Physics.Raycast(findSurfaceRay, out hit, Mathf.Infinity, terrainLayerMask))
+        {
+            GameObject.Instantiate(itemPrefab, hit.point, Quaternion.identity);
+        }
+    }
+}
