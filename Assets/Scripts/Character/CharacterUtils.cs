91536742bc8b4749b80c148f498932c53440bf28
EasternSauce <frequentmerger@gmail.com>
2025-09-13 18:50:01 +0200

    characterutils

---
+++ Assets/Scripts/Character/CharacterUtils.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CharacterUtils
+{
+    public static List<Character> GetPlayerCharacters()
+    {
+        var allCharacters = Object.FindObjectsByType<Character>(FindObjectsSortMode.None);
+        List<Character> players = new List<Character>();
+
+        foreach (var character in allCharacters)
+        {
+            if (character.GetComponent<PlayerCharacterInput>() != null)
+                players.Add(character);
+        }
+
+        return players;
+    }
+}
