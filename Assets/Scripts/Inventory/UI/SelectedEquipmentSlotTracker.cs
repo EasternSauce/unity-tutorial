f91149c1845ec4f5b9d79930298b221fd0dfa5b2
EasternSauce <frequentmerger@gmail.com>
2025-09-14 23:40:19 +0200

    show tooltip on equipment item hover

--- Assets/Scripts/Inventory/UI/SelectedEquipmentSlotTracker.cs
+++ Assets/Scripts/Inventory/UI/SelectedEquipmentSlotTracker.cs
@@ -6,20 +6,36 @@ public class SelectedEquipmentSlotTracker : MonoBehaviour, IPointerEnterHandler,
     InventoryController inventoryController;
     EquipmentItemSlot slot;
 
-#pragma warning disable CS0618
+    [SerializeField] private ItemTooltip tooltip;
+
     void Awake()
     {
         inventoryController = FindObjectOfType<InventoryController>();
         slot = GetComponent<EquipmentItemSlot>();
+
+        if (tooltip == null)
+        {
+            tooltip = FindObjectOfType<ItemTooltip>();
+            if (tooltip == null)
+                Debug.LogWarning("No ItemTooltip found in the scene!");
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         inventoryController.SelectedItemSlot = slot;
+
+        var item = slot.GetItem();
+        if (item != null && item.itemData != null)
+        {
+            string tooltipText = ItemTooltipBuilder.BuildTooltip(item.itemData);
+            tooltip?.Show(tooltipText, item.itemData.icon, true, item.gameObject);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         inventoryController.SelectedItemSlot = null;
+        tooltip?.Hide();
     }
 }
