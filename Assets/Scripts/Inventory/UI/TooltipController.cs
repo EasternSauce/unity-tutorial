b65f495f90652aa2d526736aa11c1cf6310d9dd0
EasternSauce <frequentmerger@gmail.com>
2025-09-16 19:26:03 +0200

    remove unused field

--- Assets/Scripts/Inventory/UI/TooltipController.cs
+++ Assets/Scripts/Inventory/UI/TooltipController.cs
@@ -5,50 +5,37 @@ public class TooltipController : MonoBehaviour
     [SerializeField] private ItemTooltip tooltip;
     [SerializeField] private SelectedItemController selectedItemController;
 
-    private GameObject currentHoverTarget;
 
     public void ShowTooltipForGroundItem(ItemData data, GameObject target)
     {
         if (data == null) return;
-        currentHoverTarget = target;
         tooltip?.ShowForItemData(data, false, target);
     }
 
     public void HideTooltipForGroundItem()
     {
-        currentHoverTarget = null;
         tooltip?.ForceHide();
     }
 
     public void ShowTooltipForInventoryGridItem(InventoryItem item)
     {
         if (item == null) return;
-        currentHoverTarget = item.gameObject;
         tooltip?.ShowForItem(item, true, item.gameObject);
     }
 
     public void HideTooltipForInventoryGridItem()
     {
-        currentHoverTarget = null;
         tooltip?.ForceHide();
     }
 
     public void ShowTooltipForEquipmentSlot(InventoryItem item, GameObject slot)
     {
         if (item == null || slot == null) return;
-        currentHoverTarget = slot;
         tooltip?.ShowForItem(item, true, slot);
     }
 
     public void HideTooltipForEquipmentSlot()
     {
-        currentHoverTarget = null;
-        tooltip?.ForceHide();
-    }
-
-    public void ForceHideTooltip()
-    {
-        currentHoverTarget = null;
         tooltip?.ForceHide();
     }
 }
